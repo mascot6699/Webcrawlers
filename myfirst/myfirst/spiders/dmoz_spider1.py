18907e9d9b3fbb18ccda62fbb8cc8be3c73c7dcc
Umang Shukla <umangshucool@gmail.com>
2013-10-13 19:44:04 +0530

    similar crawler
    scrapy crawl dmoz -o items.json -t json
    this crawler saves the items obtained as a json files

---
+++ myfirst/myfirst/spiders/dmoz_spider1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+
+from myfirst.items import DmozItem
+
+class DmozSpider(BaseSpider):
+   name = "dmoz1"
+   allowed_domains = ["dmoz.org"]
+   start_urls = [
+       "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+       "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+   ]
+
+   def parse(self, response):
+       hxs = HtmlXPathSelector(response)
+       sites = hxs.select('//ul/li')
+       items = []
+       for site in sites:
+           item = DmozItem()
+           item['title'] = site.select('a/text()').extract()
+           item['link'] = site.select('a/@href').extract()
+           item['desc'] = site.select('text()').extract()
+           items.append(item)
+       return items
